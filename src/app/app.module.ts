1662ba09d99e3dcbe0a27717e6fc78070ff04855
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2021-01-22 15:40:42 -0800

    initial randomizer design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RandomizerComponent } from './randomizer/randomizer.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RandomizerComponent
+    RandomizerComponent,
+    ReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/randomizer/randomizer.component.ts
+++ src/app/randomizer/randomizer.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Movie, Review, DataService } from '../data.service';
+
 @Component({
   selector: 'app-randomizer',
   templateUrl: './randomizer.component.html',
   styleUrls: ['./randomizer.component.scss']
 })
 export class RandomizerComponent implements OnInit {
+  GENRE_EMOJI_MAP = {
+    'Shit': '💩',
+    'Weeby': '🍣',
+    'Action': '🔫',
+    'Comedy': '🤣',
+    'Kung Fu': '🥋',
+    'Ghibli': '🐹',
+    'Horror': '🧟',
+    'Suspense': '😨',
+    'Drama': '🎭'
+  }
+  GENRE_EMOJI_UNKNOWN = '🍿';
 
-  constructor() { }
+  GENRE_COLOR_MAP = {
+    'Shit': '#ffedd6',
+    'Weeby': '#ffd4f4',
+    'Action': '#ffe6e6',
+    'Comedy': '#e8ffd6',
+    'Kung Fu': '#d6fff5',
+    'Ghibli': '#f9d6ff',
+    'Horror': '#b0b0b0',
+    'Suspense': '#e6e7ff',
+    'Drama': '#d6fff8'
+  }
+  GENRE_COLOR_UNKNOWN = '#eee';
+
+  movies: Movie[];
+  selected: Movie;
+
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.data.fetch().subscribe(movies => this.movies = movies);
   }
 
+  private random(options: Movie[]): Movie {
+    return options[Math.floor(Math.random()*options.length)];
+  }
+
+  select(): void {
+    this.selected = this.random(this.movies);
+  }
+
+  reset(): void {
+    this.selected = undefined;
+  }
+
+  genre_emoji(selected: string): string {
+    let emoji: string = this.GENRE_EMOJI_MAP[selected];
+
+    if(emoji === undefined) {
+      emoji = this.GENRE_EMOJI_UNKNOWN;
+    }
+
+    return emoji;
+  }
+
+  genre_color(selected: string): string {
+    let color: string = this.GENRE_COLOR_MAP[selected];
+
+    if(color === undefined) {
+      color = this.GENRE_COLOR_UNKNOWN;
+    }
+
+    return color;
+  }
 }
