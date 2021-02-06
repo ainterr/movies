c742b6c4220ec73b752ba88bb8e8e8e0379b1836
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2021-02-06 08:50:58 -0800

    added genre selection to randomizer

--- src/app/randomizer/randomizer.component.ts
+++ src/app/randomizer/randomizer.component.ts
@@ -12,14 +12,64 @@ export class RandomizerComponent {
   @Input() movies: Movie[];
   selected: Movie;
 
+  private filters: {[name: string]: boolean} = {};
+
   constructor() { }
 
   private random(options: Movie[]): Movie {
     return options[Math.floor(Math.random()*options.length)];
   }
 
+  genres(): string[] {
+    return this.movies.map(
+      m => m.genre
+    ).filter(
+      (item, pos, self) => self.indexOf(item) == pos
+    );
+  }
+
+  filter(genre: string): void {
+    this.filters[genre] = true;
+  }
+
+  unfilter(genre: string): void {
+    this.filters[genre] = false;
+  }
+
+  toggleFilter(genre: string): void {
+    this.filters[genre] = !this.filters[genre];
+  }
+
+  resetFilter(): void {
+    this.filters = {};
+  }
+
+  isFiltered(genre?: string): boolean {
+    if(genre === undefined) {
+      let filtered: boolean = false;
+
+      Object.keys(this.filters).forEach(
+        g => {
+          if(this.filters[g] === true) filtered = true;
+        }
+      )
+
+      return filtered;
+    }
+
+    return this.filters[genre] === true;
+  }
+
   select(): void {
-    this.selected = this.random(this.movies);
+    let options: Movie[] = this.movies;
+    
+    if(this.isFiltered()) {
+      options = options.filter(
+        m => this.filters[m.genre]
+      );
+    }
+
+    this.selected = this.random(options);
   }
 
   reset(): void {
